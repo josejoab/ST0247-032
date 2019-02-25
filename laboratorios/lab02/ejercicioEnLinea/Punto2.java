e2c2be5db4f908a97aaaaaa4435e11e7f178c95c
josejoab <41337937+josejoab@users.noreply.github.com>
2019-02-24 20:59:54 -0500

    Update and rename Lab2.java to Punto2.java

--- laboratorios/lab02/ejercicioEnLinea/Punto2.java
+++ laboratorios/lab02/ejercicioEnLinea/Punto2.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 /**
  *
- * @author ASUS
+ * @author Joab and Kevin
  */
-public class Lab2 {
+public class Punto2 {
 
     public static void main(String[] args) {
         leer_Tablero();
     }
-//------- ejercicio 2
+
+/**
+ * este metodo controla la entrada de casos y recibe el numero de reinas con 
+ * el que trabajara cada uno
+ */
 public static void leer_Tablero(){
 
   Scanner sc  = new Scanner(System.in);
@@ -31,6 +35,15 @@ public static void leer_Tablero(){
   }
 }
 
+/**
+ * este metodo crea una matriz n*n, siendo n la cantidad de reinas, y 
+ * lee las lineas ingresadas por cada caso, identificando las posiciones donde no se pueden
+ * poner reinas y retorna la matriz
+ * 
+ * 
+ * @param nReinas
+ * @return 
+ */
 private static int[][] leer_Tablero_Aux(int nReinas){
   Scanner sc  = new Scanner(System.in);
   int[][] tablero = new int[nReinas][nReinas];
@@ -45,12 +58,29 @@ private static int[][] leer_Tablero_Aux(int nReinas){
   return tablero;
 }
 
+/**
+ * este metodo recibe el numero de reinas y la matriz con la que se trabajara
+ * y lo envia a un metodo auxiliar
+ * @param reinas numero de reinas
+ * @param tablero matriz que representa el tablero
+ * @return 
+ */
 public static int nReinas(int reinas, int[][] tablero){
   int[] posicion = new int[reinas];
   int[] suma = new int[1];
    return nReinas_Aux(reinas, tablero, 0, posicion, suma);
 }
 
+/**
+ * este metodo calcula el numero de soluciones que puede tener el problema de las n reinas 
+ * con un n numero determinado y sin usar las posiciones malas
+ * @param nReinas numero de reinas
+ * @param tablero matriz que representa el tablero de ajedrez
+ * @param col numero que controla el indice del arreglo "posicion" y representa las columnas de la matriz
+ * @param posicion arreglo que contendra el numero de filas en el que se encuentran las reinas
+ * @param suma arreglo que se utiliza como contador de cada solucion 
+ * @return la cantidad de soluciones
+ */
 private static int nReinas_Aux(int nReinas, int[][] tablero, int col, int[] posicion,int[] suma){
   if(col>= nReinas && seAtacan(posicion,nReinas)){
     suma[0] += 1;
@@ -63,12 +93,16 @@ private static int nReinas_Aux(int nReinas, int[][] tablero, int col, int[] posi
         }
     }
   }
-       //System.out.println("suma :"+suma[0]);
-      // imprimirTablero(posicion);
+
        return suma[0];
  }
 
-
+/**
+ * metodo que verifica si en la matriz las reinas se atacan
+ * @param tablero arreglo que representa el tablero (matriz)
+ * @param elI numero de posiciones
+ * @return true si no se atacan
+ */
      private static boolean seAtacan(int [ ] tablero, int elI){
         for(int i = 0; i <= elI-1; i++){
             for(int j = i+1; j < elI; j++){
@@ -79,7 +113,10 @@ private static int nReinas_Aux(int nReinas, int[][] tablero, int col, int[] posi
         }
         return true;
     }
-     
+     /**
+      * metodo que imprime el tablero de juego
+      * @param tablero 
+      */
       public static void imprimirTablero(int[] tablero) {
     int n = tablero.length;
     System.out.print("    ");
