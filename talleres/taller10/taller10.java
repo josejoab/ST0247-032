2d84c61cd3f1f1c3fa4a8e8536a5ae1327c8ec50
josejoab <41337937+josejoab@users.noreply.github.com>
2019-04-04 11:40:08 -0500

    Create taller10.java

---
+++ talleres/taller10/taller10.java
@@ -0,0 +1,16 @@
+    public static int lcs(String x, String y) {
+       int[][] laTabla = int[x.length()+1][y.length()+1];
+       for(int i = 0; i <= x.length; i++)
+          laTabla[i][0] = 0;
+       for(int j = 0; j <= y.length; j++)
+          laTabla[0][j] = 0;
+       
+       for (int i = 1; i <= x.length; i++)
+         for (int j = 1; j <= y.length; j++){
+             if (x.charAt(i-1) == y.charAt(j-1))
+               laTabla[i][j] = laTabla[i-1][j-1] + 1;
+             else 
+               laTabla[i][j] = Math.max(laTabla[i][j-1], laTabla[i-1][j]);
+            }
+       return laTabla[x.length][y.length];  
+}
